1d224cba3729ae9ae789b8e75eb10e70cc66f12c
Andikasaputra26 <83541363+Andikasaputra26@users.noreply.github.com>
2022-07-04 19:11:52 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restoran Malebbie</title>
+    <link rel="icon" href="image/banner-3.png">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<header class="header">
+
+<a href="#" class="logo"> <i class="fas fa-utensils"></i> Malebbie </a>
+
+<nav class="navbar">
+
+        
+    
+    
+</nav>
+
+<div class="icons">
+    <div id="menu-btn" class="fas fa-bars"></div>
+    <div id="search-btn" class="fas fa-search"></div>
+    <div id="cart-btn" class="fas fa-shopping-cart"></div>
+    <div id="login-btn" class="fas fa-user"></div>
+</div>
+
+</header>
+
+
+<div class="login-form-container">
+
+    <form action="">
+        <h3>login form</h3>
+        <input type="email" name="" placeholder="enter your email" id="" class="box">
+        <input type="password" name="" placeholder="enter your password" id="" class="box">
+        <div class="remember">
+            <input type="checkbox" name="" id="remember-me">
+            <label for="remember-me">Lupa password</label>
+        </div>
+        <input type="submit" value="Login Sekarang" class="btn"> 
+        <p>forget password? <a href="#">click here</a></p>
+        <p>don't have an account? <a href="#">create one</a></p>
+    </form>
+
+</div>
+<script src="js/ora.js"></script>
+</body>
+</html>
